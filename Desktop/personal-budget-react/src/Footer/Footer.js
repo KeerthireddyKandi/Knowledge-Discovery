5bef8558dba708a1e795fe320cfaec03f3da918f
Keerthi Reddy <keerthireddy@Keerthis-MacBook-Pro.local>
2020-10-15 16:53:30 -0400

    Brought personal budget code

---
+++ Desktop/personal-budget-react/src/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div className="bottom">
+        <div className="center">
+
+            <div className='lastbtn col-lg-6'>
+                <button type="button" className='btn btn-dark btn-lg fab fa-apple download'> Download</button>
+                <button type="button" className='btn btn-outline-light btn-lg fab fa-google-play download'> Download</button>
+              </div>
+            <hr>
+</hr>
+            All rights reserved &copy; Keerthi Reddy Kandi
+        </div>
+    </div>
+
+  );
+}
+
+export default Footer;
